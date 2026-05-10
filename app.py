2aa0667a4dc95fdee3ea326807ddb5e182766db2
janani703priya-hash <janani703priya@gmail.com>
2026-05-10 13:20:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,8 @@ def delete_task(task_id):
     db.session.commit()
     return jsonify({"message": "Task deleted"})
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(host="0.0.0.0", port=5000)
